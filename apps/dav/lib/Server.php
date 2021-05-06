e735d40e5f4c58c79853fce87c5749208d8b8330
Julius Härtl <jus@bitgrid.net>
2021-05-06 18:26:42 +0200

    First attempt to make multipartPart upload working
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- apps/dav/lib/Server.php
+++ apps/dav/lib/Server.php
@@ -74,6 +74,7 @@ use OCA\DAV\BulkUpload\BulkUploadPlugin;
 use OCA\DAV\Provisioning\Apple\AppleProvisioningPlugin;
 use OCA\DAV\SystemTag\SystemTagPlugin;
 use OCA\DAV\Upload\ChunkingPlugin;
+use OCA\DAV\Upload\ChunkingV2Plugin;
 use OCP\EventDispatcher\IEventDispatcher;
 use OCP\IRequest;
 use OCP\SabrePluginEvent;
@@ -214,7 +215,7 @@ class Server {
 		));
 
 		$this->server->addPlugin(new CopyEtagHeaderPlugin());
-		$this->server->addPlugin(new RequestIdHeaderPlugin(\OC::$server->get(IRequest::class)));
+		$this->server->addPlugin(new ChunkingV2Plugin());
 		$this->server->addPlugin(new ChunkingPlugin());
 
 		// allow setup of additional plugins
