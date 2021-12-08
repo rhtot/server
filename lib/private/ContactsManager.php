de208ef0b4ff40f62c4c7b17a71eb71904ec91df
Joas Schilling <coding@schilljs.com>
2021-12-08 16:59:32 +0100

    Only limit search in the system address book
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/private/ContactsManager.php
+++ lib/private/ContactsManager.php
@@ -42,7 +42,8 @@ class ContactsManager implements IManager {
 	 * 	- 'escape_like_param' - If set to false wildcards _ and % are not escaped
 	 * 	- 'limit' - Set a numeric limit for the search results
 	 * 	- 'offset' - Set the offset for the limited search results
-	 * 	- 'wildcard' - Whether the search should use wildcards
+	 * 	- 'enumeration' - Whether user enumeration on system address book is allowed
+	 * 	- 'fullmatch' - Whether matching on full detail in system address book is allowed
 	 * @return array an array of contacts which are arrays of key-value-pairs
 	 */
 	public function search($pattern, $searchProperties = [], $options = []) {
@@ -50,7 +51,6 @@ class ContactsManager implements IManager {
 		$result = [];
 		foreach ($this->addressBooks as $addressBook) {
 			$searchOptions = $options;
-			$strictSearch = array_key_exists('strict_search', $options) && $options['strict_search'] === true;
 
 			if ($addressBook->isSystemAddressBook()) {
 				$fullMatch = !\array_key_exists('fullmatch', $options) || $options['fullmatch'] !== false;
@@ -58,13 +58,7 @@ class ContactsManager implements IManager {
 					// Neither full match is allowed, so skip the system address book
 					continue;
 				}
-				if ($strictSearch) {
-					$searchOptions['wildcard'] = false;
-				} else {
-					$searchOptions['wildcard'] = !\array_key_exists('enumeration', $options) || $options['enumeration'] !== false;
-				}
-			} else {
-				$searchOptions['wildcard'] = !$strictSearch;
+				$searchOptions['wildcard'] = !\array_key_exists('enumeration', $options) || $options['enumeration'] !== false;
 			}
 
 			$r = $addressBook->search($pattern, $searchProperties, $searchOptions);
