57bb7c602b3a739b9ad0865c53fe48ad517743fd
sangramsinh.desai@t-systems.com <sangramsinh.desai@t-systems.com>
2021-12-15 17:06:00 +0530

    optional parameter exception handled for displayname

--- lib/private/Share20/Manager.php
+++ lib/private/Share20/Manager.php
@@ -837,7 +837,9 @@ class Manager implements IManager {
 							$this->urlGenerator->linkToRouteAbsolute('files_sharing.Accept.accept', ['shareId' => $share->getFullId()]),
 							$share->getSharedBy(),
 							$emailAddress,
-							$share->getExpirationDate()
+							$share->getExpirationDate(),
+							$share->getNote(),
+							$user->getDisplayNameOtherUser()
 						);
 						$this->logger->debug('Sent share notification to ' . $emailAddress . ' for share with ID ' . $share->getId(), ['app' => 'share']);
 					} else {
@@ -871,7 +873,9 @@ class Manager implements IManager {
 											$link,
 											$initiator,
 											$shareWith,
-											\DateTime $expiration = null) {
+											\DateTime $expiration = null,
+											$note = null,
+											$shareWithDisplayName = null) {
 		$initiatorUser = $this->userManager->get($initiator);
 		$initiatorDisplayName = ($initiatorUser instanceof IUser) ? $initiatorUser->getDisplayName() : $initiator;
 
@@ -883,6 +887,7 @@ class Manager implements IManager {
 			'initiator' => $initiatorDisplayName,
 			'expiration' => $expiration,
 			'shareWith' => $shareWith,
+			'shareWithDisplayName' => $shareWithDisplayName
 		]);
 
 		$emailTemplate->setSubject($l->t('%1$s shared »%2$s« with you', [$initiatorDisplayName, $filename]));
@@ -890,6 +895,7 @@ class Manager implements IManager {
 		$emailTemplate->addHeading($l->t('%1$s shared »%2$s« with you', [$initiatorDisplayName, $filename]), false);
 		$text = $l->t('%1$s shared »%2$s« with you.', [$initiatorDisplayName, $filename]);
 
+		$emailTemplate->addBodyText(htmlspecialchars($note), $note);
 		$emailTemplate->addBodyText(
 			htmlspecialchars($text . ' ' . $l->t('Click the button below to open it.')),
 			$text
