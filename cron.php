3b291c7ffbc5c0b0982c2d8d20e98408b056a1c3
ashish <ashish.pathak@t-systems.com>
2022-09-06 12:34:51 +0530

    Allow calling cron jobs background job class with occ

--- cron.php
+++ cron.php
@@ -64,13 +64,14 @@ try {
 
 	$logger = \OC::$server->getLogger();
 	$config = \OC::$server->getConfig();
+	$tempManager = \OC::$server->getTempManager();
 
 	// Don't do anything if Nextcloud has not been installed
 	if (!$config->getSystemValue('installed', false)) {
 		exit(0);
 	}
 
-	\OC::$server->getTempManager()->cleanOld();
+	$tempManager->cleanOld();
 
 	// Exit if background jobs are disabled!
 	$appMode = $config->getAppValue('core', 'backgroundjobs_mode', 'ajax');
@@ -141,7 +142,8 @@ try {
 		$endTime = time() + 14 * 60;
 
 		$executedJobs = [];
-		while ($job = $jobList->getNext($onlyTimeSensitive)) {
+		$jobClass = isset($argv[1]) ? $argv[1] : null;
+		while ($job = $jobList->getNext($onlyTimeSensitive, $jobClass)) {
 			if (isset($executedJobs[$job->getId()])) {
 				$jobList->unlockJob($job);
 				break;
@@ -149,8 +151,10 @@ try {
 
 			$logger->debug('CLI cron call has selected job with ID ' . strval($job->getId()), ['app' => 'cron']);
 			$job->execute($jobList, $logger);
+
 			// clean up after unclean jobs
 			\OC_Util::tearDownFS();
+			$tempManager->clean();
 
 			$jobList->setLastJob($job);
 			$executedJobs[$job->getId()] = true;
