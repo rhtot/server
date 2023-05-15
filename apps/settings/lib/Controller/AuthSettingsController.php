146f0c0ffabdf09b10702784fd7c6564741b7aff
Bernd.Rederlechner@t-systems.com <bernd.rederlechner@t-systems.com>
2023-05-15 18:46:30 +0200

    Build a simple, artificial backport example

--- apps/settings/lib/Controller/AuthSettingsController.php
+++ apps/settings/lib/Controller/AuthSettingsController.php
@@ -157,9 +157,10 @@ class AuthSettingsController extends Controller {
 
 		$this->publishActivity(Provider::APP_TOKEN_CREATED, $deviceToken->getId(), ['name' => $deviceToken->getName()]);
 
+		$user = $this->userSession->getUser(); // mark backport
 		return new JSONResponse([
 			'token' => $token,
-			'loginName' => $loginName,
+			'loginName' => $user->getEMailAddress(),
 			'deviceToken' => $tokenData,
 		]);
 	}
